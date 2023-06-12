56cf28639b91b3aa6e49db012a3fcb72561942c2
karolina.kukowska@gmail.com <64924406+karola-kasprzak@users.noreply.github.com>
2023-06-12 17:48:23 +0200

    sidebar logic

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,43 @@
+import { useGlobalContext } from "../context";
+import logo from "../assets/logo.svg";
+import { FaTimes } from "react-icons/fa";
+import { links, social } from "../data";
+
 const Sidebar = () => {
-  return <div>Sidebar</div>;
+  const { isSidebarOpen, closeSidebar } = useGlobalContext();
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-header">
+        <img src={logo} alt="coding addict" />
+        <button type="button" onClick={closeSidebar} className="close-btn">
+          <FaTimes />
+        </button>
+      </div>
+      <ul className="links">
+        {links.map((link) => {
+          const { id, url, text, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>
+                {icon}
+                {text}
+              </a>
+            </li>
+          );
+        })}
+      </ul>
+      <ul className="social-links">
+        {social.map((link) => {
+          const { id, url, icon } = link;
+          return (
+            <li key={id}>
+              <a href={url}>{icon}</a>
+            </li>
+          );
+        })}
+      </ul>
+    </aside>
+  );
 };
 export default Sidebar;
